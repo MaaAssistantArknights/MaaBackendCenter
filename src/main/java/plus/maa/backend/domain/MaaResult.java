db7feab73e2592244b3df2aa252b97b4bf5d903c
Ansel <2894137614@qq.com>
2022-12-22 13:25:36 +0800

    修改响应类

--- src/main/java/plus/maa/backend/domain/MaaResult.java
+++ src/main/java/plus/maa/backend/domain/MaaResult.java
@@ -1,45 +1,32 @@
 package plus.maa.backend.domain;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
- * Maa的响应类，注意不要加Lombok的注解，格式已适配前端的请求
- *
  * @author AnselYuki
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MaaResult {
-    private final Map<Object, Object> result;
-    private final String code;
+    @JsonAlias("status_code")
+    private Integer statusCode;
+    private String message;
+    private Object data;
 
-    public MaaResult(int code, String msg) {
-        this.code = String.valueOf(code);
-        Map<Object, Object> result = new HashMap<>(3);
-        result.put("status_code", code);
-        result.put("message", msg);
-        this.result = result;
+    public MaaResult(int statusCode, String message, Object data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
     }
 
-    public MaaResult(int code, String msg, Object data) {
-        this.code = String.valueOf(code);
-        Map<Object, Object> result = new HashMap<>(3);
-        result.put("status_code", code);
-        result.put("message", msg);
-        result.put("data", data);
-        this.result = result;
-    }
-
-    /**
-     * 动态修改json数组的键名，要返回的数据记得封装在result内
-     * 由于前端的接口请求格式，所有数据都以httpCode为键，作为一个单集合被序列化
-     *
-     * @return 修改完毕的对象
-     */
-    @JsonAnyGetter
-    public Map<String, Object> getResultMap() {
-        return Collections.singletonMap(code, result);
+    public MaaResult(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
     }
 }
