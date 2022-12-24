b767eae6de49548b56847f3c59dd8ed5cf5bf1bd
lomu <2842775752@qq.com>
2022-12-24 16:05:13 +0800

    修改为LocalDateTime.now()

--- src/main/java/plus/maa/backend/controller/SystemController.java
+++ src/main/java/plus/maa/backend/controller/SystemController.java
@@ -3,14 +3,16 @@ package plus.maa.backend.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import plus.maa.backend.common.utils.EmailUtils;
 import plus.maa.backend.controller.response.MaaResult;
+import plus.maa.backend.service.EmailService;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +30,7 @@ public class SystemController {
     @Value("${maa-copilot.info.description}")
     private String description;
 
+    private final EmailService emailService;
 
 
     @GetMapping("/")
@@ -38,15 +41,15 @@ public class SystemController {
     @GetMapping("version")
     public MaaResult<Map<String,String>> version(){
         Map<String,String> map = new HashMap<>();
-        map.put("time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS")
-                .format(System.currentTimeMillis()));
+        map.put("time", LocalDateTime.now()
+                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS")));
         map.put("version",version);
         return MaaResult.success(map);
     }
 
     @GetMapping("test/email")
     public  MaaResult<String> testEmail(){
-        new EmailUtils().TestEmail();
+         emailService.sendVCode("@qq.com");
         return MaaResult.success("发送成功");
     }
 }
