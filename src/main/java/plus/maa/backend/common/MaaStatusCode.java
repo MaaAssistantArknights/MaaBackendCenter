3d15cfd27694ba601fa05588edfdc7dcde28b8a3
Ansel <2894137614@qq.com>
2022-12-28 21:21:25 +0800

    变更状态码，细分Http500错误

--- src/main/java/plus/maa/backend/common/MaaStatusCode.java
+++ src/main/java/plus/maa/backend/common/MaaStatusCode.java
@@ -10,8 +10,9 @@ public enum MaaStatusCode {
     /**
      * MAA自定义状态码
      */
-    MAA_USER_NOT_FOUND(10002, "找不到用户"),
-    MAA_ACTIVE_ERROR(10003, "激活失败");
+    MAA_USER_NOT_FOUND(10001, "找不到用户"),
+    MAA_ACTIVE_ERROR(10002, "激活失败"),
+    MAA_USER_NOT_ENABLED(10003, "用户未启用");
     public final int code;
     public final String message;
 }
