27078f3d708c99e275708321f96fb0f5271846db
LinTx <lintx@lintx.org>
2022-12-29 14:23:49 +0800

    活动地图cat two增加区域信息

--- src/main/java/plus/maa/backend/repository/entity/arklevel/parse/ActivitiesLevelParse.java
+++ src/main/java/plus/maa/backend/repository/entity/arklevel/parse/ActivitiesLevelParse.java
@@ -2,11 +2,13 @@ package plus.maa.backend.repository.entity.arklevel.parse;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import plus.maa.backend.repository.entity.ArkLevel;
 import plus.maa.backend.repository.entity.arklevel.LevelParse;
 import plus.maa.backend.repository.entity.gamedata.ArkActivity;
 import plus.maa.backend.repository.entity.gamedata.ArkStage;
 import plus.maa.backend.repository.entity.gamedata.ArkTilePos;
+import plus.maa.backend.repository.entity.gamedata.ArkZone;
 
 @Slf4j
 @Component
@@ -16,6 +18,7 @@ public class ActivitiesLevelParse extends LevelParse {
      * Activity -> ACT_NAME -> StageCode == activities/ACT_ID/LEVEL_ID<br>
      * eg:<br>
      * 活动关卡 -> 战地秘闻 -> SW-EV-1 == activities/act4d0/level_act4d0_01<br>
+     * 活动关卡 -> 照我以火 荒烟林沼 -> FC-1 == activities/act22side/level_act22side_01
      */
     @Override
     public void parse(ArkLevel level, ArkTilePos tilePos) {
@@ -32,7 +35,25 @@ public class ActivitiesLevelParse extends LevelParse {
             log.error("[PARSER]活动关卡未找到activity信息:{}", level.getLevelId());
             return;
         }
+        ArkZone zone = dataService.findZone(level.getLevelId(), tilePos.getCode(), tilePos.getStageId());
+        if (zone == null) {
+            return;
+        }
+        level.setCatTwo(parseZoneName(act, zone));
+    }
 
-        level.setCatTwo(act.getName());
+    private String parseZoneName(ArkActivity act, ArkZone zone) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(act.getName());
+        if (zone == null) return builder.toString().trim();
+        if (!ObjectUtils.isEmpty(zone.getZoneNameFirst())) {
+            builder.append(" ");
+            builder.append(zone.getZoneNameFirst());
+        }
+        if (!ObjectUtils.isEmpty(zone.getZoneNameSecond())) {
+            builder.append(" ");
+            builder.append(zone.getZoneNameSecond());
+        }
+        return builder.toString().trim();
     }
 }
