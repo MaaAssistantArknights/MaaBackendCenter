4676564cf5f2cc820c018b01adbcc3c9b5f83edc
dragove <dragove@qq.com>
2023-01-18 23:29:29 +0800

    fix: 修复数据同步不及时的问题

--- src/main/java/plus/maa/backend/task/ArkLevelSyncTask.java
+++ src/main/java/plus/maa/backend/task/ArkLevelSyncTask.java
@@ -1,6 +1,8 @@
 package plus.maa.backend.task;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import plus.maa.backend.service.ArkLevelService;
@@ -12,9 +14,11 @@ public class ArkLevelSyncTask {
     private final ArkLevelService arkLevelService;
 
     /**
-     * 地图数据同步定时任务，cron当前的意思为每小时执行一次
+     * 地图数据同步定时任务，cron当前的意思为每10分钟执行一次
+     * 应用启动时自动同步一次
      */
-    @Scheduled(cron = "0 0 */1 * * ?")
+    @EventListener(ApplicationReadyEvent.class)
+    @Scheduled(cron = "0 0/10 * * * ? ")
     public void syncArkLevels() {
         arkLevelService.runSyncLevelDataTask();
     }
