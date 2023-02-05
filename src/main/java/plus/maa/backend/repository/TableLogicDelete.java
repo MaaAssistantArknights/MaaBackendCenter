b63a7f7cd5660e02454db498e46a9a74f9424e6e
dragove <dragove@qq.com>
2023-02-05 16:39:16 +0800

    fix: 修复 TableLogicDelete 编译错误

--- src/main/java/plus/maa/backend/repository/TableLogicDelete.java
+++ src/main/java/plus/maa/backend/repository/TableLogicDelete.java
@@ -1,18 +1,18 @@
 package plus.maa.backend.repository;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
+
+import lombok.RequiredArgsConstructor;
 import plus.maa.backend.controller.response.MaaResultException;
 import plus.maa.backend.repository.entity.Copilot;
 import plus.maa.backend.repository.entity.CopilotRating;
 
-import java.util.Date;
-import java.util.Optional;
-
 /**
  * @author LoMu
- * Date  2023-01-20 14:59
+ *         Date 2023-01-20 14:59
  */
 @Component
 @RequiredArgsConstructor
@@ -22,18 +22,20 @@ public class TableLogicDelete {
 
     public void deleteCopilotById(String id) {
         Optional<Copilot> byId = copilotRepository.findById(id);
-        if (byId.isEmpty()) throw new MaaResultException("copilot Id不存在");
+        if (byId.isEmpty())
+            throw new MaaResultException("copilot Id不存在");
         Date date = new Date();
         Copilot copilot = byId.get();
         copilot.setDelete(true);
         copilot.setDeleteTime(date);
         copilotRepository.save(copilot);
 
-        CopilotRating copilotRating = copilotRatingRepository.findByCopilotId(id);
-        if (ObjectUtils.isEmpty(copilotRating)) throw new RuntimeException("copilot rating is null");
-        copilotRating.setDelete(true);
-        copilotRating.setDeleteTime(date);
-        copilotRatingRepository.save(copilotRating);
+        Optional<CopilotRating> copilotRating = copilotRatingRepository.findByCopilotId(Long.valueOf(id));
+        copilotRating.ifPresent(rating -> {
+            rating.setDelete(true);
+            rating.setDeleteTime(date);
+            copilotRatingRepository.save(rating);
+        });
 
     }
 }
