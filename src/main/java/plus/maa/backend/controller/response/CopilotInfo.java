bb2a08580282312fc7460199ddbaed605ef00be7
dragove <dragove@qq.com>
2023-02-11 14:38:01 +0800

    feat: 简化copilotInfo返回内容，前端从content字段获取数据

--- src/main/java/plus/maa/backend/controller/response/CopilotInfo.java
+++ src/main/java/plus/maa/backend/controller/response/CopilotInfo.java
@@ -3,36 +3,25 @@ package plus.maa.backend.controller.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import plus.maa.backend.repository.entity.Copilot;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CopilotInfo implements Serializable {
-    private String id;
-    private Long copilotId;
-    private String minimumRequired;
+    private Long id;
 
     private Date uploadTime;
-    private String title;
-    private String detail;
     private String uploader;
     //用于前端显示的格式化后的干员信息 [干员名]::[技能]
-    private List<String> operators;
-    private List<String> opers;
-    private List<Copilot.Groups> groups;
     private int views;
     private int hotScore;
-    private ArkLevelInfo level;
     private boolean available;
     private int ratingLevel;
     private boolean isNotEnoughRating;
     private double ratingRatio;
     private int ratingType;
-    private int difficulty;
     private String content;
 }
