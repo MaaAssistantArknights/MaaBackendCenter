7868a38b2988d29e0862b7e43a57f7a9674fa742
lixuhuilll <676824363@qq.com>
2023-08-30 17:30:58 +0800

    调整 incZSet.lua 的逻辑，使其过期时间的设置行为和 RedisCache 中其他类似方法相同

--- src/main/resources/redis-lua/incZSet.lua
+++ src/main/resources/redis-lua/incZSet.lua
@@ -1,10 +1,12 @@
 local member = ARGV[1]
 local incScore = ARGV[2]
-local size = ARGV[3]
-local timeout = ARGV[4]
+local size = tonumber(ARGV[3])
+local timeout = tonumber(ARGV[4])
 redis.call('ZINCRBY', KEYS[1], incScore, member)
-redis.call('EXPIRE', KEYS[1], timeout)
+if timeout > 0 then -- timeout 必须是数字
+    redis.call('EXPIRE', KEYS[1], timeout)
+end
 local count = redis.call('ZCARD', KEYS[1])
-if count > size + 50 then
+if count > size + 50 then   -- 这里 size 就算不转数字也能用，是因为部分运算会自动转换字符串
     redis.call('ZREMRANGEBYRANK', KEYS[1], 0, count - size - 1)
 end
