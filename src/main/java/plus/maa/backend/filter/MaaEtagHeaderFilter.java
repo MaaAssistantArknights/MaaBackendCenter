ed078acc4bda1851fe63f0a57454dcf3254a6940
lixuhuilll <676824363@qq.com>
2023-10-11 02:14:27 +0800

    撤销 ArkLevel 的特殊处理，考虑到如果服务出现异常响应，这种在生成响应前就决定 ETag 的行为可能导致缓存异常数据，还是不要为这点性能冒险，能节约网络带宽就足够了

--- src/main/java/plus/maa/backend/filter/MaaEtagHeaderFilter.java
+++ src/main/java/plus/maa/backend/filter/MaaEtagHeaderFilter.java
@@ -7,10 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import plus.maa.backend.repository.RedisCache;
 
 import java.io.IOException;
 
@@ -29,25 +27,15 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class MaaEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
-    private final RedisCache redisCache;
-
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
-        // 允许使用 Etag （实际是避免默认添加的 no-store）
-        response.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate");
-        // 为 ArkLevel 进行特殊处理，因为它在 Redis 中存在指示变更的值，可以借此节约性能
-        if ("/arknights/level".equals(request.getRequestURI())) {
-            String levelCommit = redisCache.getCacheLevelCommit();
-            levelCommit = levelCommit == null ? "0" : levelCommit;
-            WebRequest webRequest = new ServletWebRequest(request, response);
-            if (webRequest.checkNotModified(levelCommit)) {
-                // 只要 Redis 中指示的 levelCommit 未变更，则认为可以使用浏览器缓存的地图数据
-                return;
-            }
-            // levelCommit 发生变更或当前用户不存在缓存，响应新数据
+        if (!HttpMethod.GET.matches(request.getMethod())) {
+            // ETag 只处理安全的请求
             filterChain.doFilter(request, response);
             return;
         }
+        // 允许使用 Etag （实际是避免默认添加的 no-store）
+        response.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate");
         // 其他接口默认处理即可，注意默认操作相当于牺牲 CPU 来节约网络带宽，不适用于结果变更过快的接口
         super.doFilterInternal(request, response, filterChain);
     }
