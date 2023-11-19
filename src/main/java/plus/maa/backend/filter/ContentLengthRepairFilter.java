041fd20fd6bce7df6690fa8a6b84cfa04d5b1087
lixuhuilll <676824363@qq.com>
2023-11-19 18:35:44 +0800

    功能拆分，将 ETag 和获取 Content-Length 分为两个过滤器

---
+++ src/main/java/plus/maa/backend/filter/ContentLengthRepairFilter.java
@@ -0,0 +1,39 @@
+package plus.maa.backend.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 解决了 GZIP 无法对 JSON 响应正常处理 min-response-size 的问题，
+ * 借助了 ETag 处理流程中的 Response 包装类包装所有响应，
+ * 从而正常获取 Content-Length
+ *
+ * @author lixuhuilll
+ */
+
+@Component
+public class ContentLengthRepairFilter extends ShallowEtagHeaderFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (response instanceof ContentCachingResponseWrapper) {
+            // 不对已包装过的响应体做处理
+            filterChain.doFilter(request, response);
+        } else {
+            super.doFilterInternal(request, response, filterChain);
+        }
+    }
+
+    @Override
+    protected boolean isEligibleForEtag(HttpServletRequest request, HttpServletResponse response, int responseStatusCode, InputStream inputStream) {
+        return false;
+    }
+}
