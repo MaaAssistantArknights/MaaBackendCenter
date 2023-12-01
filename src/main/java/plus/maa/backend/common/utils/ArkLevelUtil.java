cdd199bf3a11d70f73a4d0af697882d7eec24243
lixuhuilll <676824363@qq.com>
2023-12-01 23:57:16 +0800

    修改正则表达式。优化测试用例。

--- src/main/java/plus/maa/backend/common/utils/ArkLevelUtil.java
+++ src/main/java/plus/maa/backend/common/utils/ArkLevelUtil.java
@@ -9,7 +9,7 @@ public class ArkLevelUtil {
 
     private static final Pattern NOT_KEY_INFO = Pattern.compile(
             // level_、各种难度前缀、season_、前导零、以-或者_划分的后缀
-            "^level_|^easy_|^hard_|^tough_|^main_|season_|(?<!\\d)0+(?=\\d)|[-_]+[^-_]+($|[-_]+([a-z]|perm)$)"
+            "^level_|^easy_|^hard_|^tough_|^main_|season_|(?<!\\d)0+(?=\\d)|[-_]+[^-_]+($|[-_]+\\D+$)"
     );
 
     /**
--- src/test/java/plus/maa/backend/common/utils/ArkLevelUtilTest.java
+++ src/test/java/plus/maa/backend/common/utils/ArkLevelUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static java.util.Map.entry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static plus.maa.backend.common.utils.ArkLevelUtil.getKeyInfoById;
 
@@ -11,17 +12,41 @@ class ArkLevelUtilTest {
 
     @Test
     void testGetKeyInfoById() {
-        Map<String, String> ids = Map.of(
-                "level_rune_09-01", "level_rune_09-02",
-                "level_crisis_v2_01-07", "crisis_v2_season_1_1",
-                "a001_01_perm", "a001_ex05",
-                "act11d0_ex08#f#", "act11d0_s02",
-                "act11mini_03#f#", "act11mini_04",
-                "act17side_01", "act17side_s01_a"
+
+        var ids = Map.ofEntries(
+                entry("level_rune_09-01", "level_rune_09-02"),
+                entry("level_crisis_v2_01-07", "crisis_v2_season_1_1"),
+                entry("a001_01_perm", "a001_ex05"),
+                entry("act11d0_ex08#f#", "act11d0_s02"),
+                entry("act11mini_03#f#", "act11mini_04"),
+                entry("act17side_01", "act17side_s01_a"),
+                entry("act17side_01_rep", "act17side_02_perm")
+        );
+
+        var idsWithInfo = Map.ofEntries(
+                entry("level_rune_09-01", "rune_9"),
+                entry("level_crisis_v2_01-07", "crisis_v2_1"),
+                entry("a001_01_perm", "a1"),
+                entry("act11d0_ex08#f#", "act11d0"),
+                entry("act11mini_03#f#", "act11mini"),
+                entry("act17side_01", "act17side"),
+                entry("act17side_01_rep", "act17side")
         );
 
         for (var entity : ids.entrySet()) {
-            assertEquals(getKeyInfoById(entity.getKey()), getKeyInfoById(entity.getValue()));
+            var key = entity.getKey();
+            var value = entity.getValue();
+            var infoOfKey = getKeyInfoById(key);
+            assertEquals(
+                    infoOfKey, getKeyInfoById(value),
+                    () -> key + " 与 " + value + " 的地图标识不相同"
+            );
+
+            var infoOfMap = idsWithInfo.get(key);
+            assertEquals(
+                    infoOfKey, infoOfMap,
+                    () -> key + " 的地图标识不为 " + infoOfMap
+            );
         }
     }
 }
