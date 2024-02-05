42bc972a13ca5b67ae1de409ac647dd6f60797ae
KevinT3Hu <kevint3hu@outlook.com>
2024-02-05 16:12:02 +0800

    fix: null in ArkStage deserialization

--- src/main/java/plus/maa/backend/repository/entity/gamedata/ArkStage.kt
+++ src/main/java/plus/maa/backend/repository/entity/gamedata/ArkStage.kt
@@ -5,7 +5,7 @@ data class ArkStage(
      * 关卡ID, 需转换为全小写后使用<br></br>
      * 例: Activities/ACT5D0/level_act5d0_ex08
      */
-    val levelId: String,
+    val levelId: String?,
 
     /**
      * 例: act14d7_zone2
--- src/main/java/plus/maa/backend/service/ArkGameDataService.kt
+++ src/main/java/plus/maa/backend/service/ArkGameDataService.kt
@@ -2,12 +2,11 @@ package plus.maa.backend.service
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 import io.github.oshai.kotlinlogging.KotlinLogging
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.springframework.stereotype.Service
-import org.springframework.util.ObjectUtils
 import plus.maa.backend.common.utils.ArkLevelUtil
 import plus.maa.backend.repository.entity.gamedata.*
 import java.util.*
@@ -22,15 +21,21 @@ private val log = KotlinLogging.logger {}
 class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     companion object {
-        private const val ARK_STAGE = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/stage_table.json"
-        private const val ARK_ZONE = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/zone_table.json"
-        private const val ARK_ACTIVITY = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/activity_table.json"
-        private const val ARK_CHARACTER = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/character_table.json"
-        private const val ARK_TOWER = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/climb_tower_table.json"
-        private const val ARK_CRISIS_V2 = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/crisis_v2_table.json"
+        private const val ARK_STAGE =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/stage_table.json"
+        private const val ARK_ZONE =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/zone_table.json"
+        private const val ARK_ACTIVITY =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/activity_table.json"
+        private const val ARK_CHARACTER =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/character_table.json"
+        private const val ARK_TOWER =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/climb_tower_table.json"
+        private const val ARK_CRISIS_V2 =
+            "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/gamedata/excel/crisis_v2_table.json"
     }
 
-    private final val mapper = JsonMapper.builder()
+    private final val mapper = jacksonMapperBuilder()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .build()
     private val stageMap = ConcurrentHashMap<String, ArkStage>()
@@ -41,7 +46,7 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
     private val arkTowerMap = ConcurrentHashMap<String, ArkTower>()
     private val arkCrisisV2InfoMap = ConcurrentHashMap<String, ArkCrisisV2Info>()
 
-    fun syncGameData(): Boolean{
+    fun syncGameData(): Boolean {
         return syncStage() &&
                 syncZone() &&
                 syncActivity() &&
@@ -50,7 +55,7 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                 syncCrisisV2Info()
     }
 
-    fun findStage(levelId:String,code:String,stageId:String):ArkStage? {
+    fun findStage(levelId: String, code: String, stageId: String): ArkStage? {
         val stage = levelStageMap[levelId.lowercase(Locale.getDefault())]
         if (stage != null && stage.code.equals(code, ignoreCase = true)) {
             return stage
@@ -99,11 +104,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     private fun syncStage(): Boolean {
         val req = Request.Builder().url(ARK_STAGE).get().build()
-
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取stage数据失败" }
                     return false
                 }
@@ -114,25 +118,25 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                 levelStageMap.clear()
                 temp.forEach { (k, v) ->
                     stageMap[k] = v
-                    if (!ObjectUtils.isEmpty(v.levelId)) {
-                        levelStageMap[v.levelId.lowercase(Locale.getDefault())] = v
+                    v.levelId?.let {
+                        levelStageMap[it.lowercase(Locale.getDefault())] = v
                     }
                 }
                 log.info { "${"[DATA]获取stage数据成功, 共{}条"} ${levelStageMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步stage数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步stage数据异常" }
+            return false
         }
         return true
     }
 
     private fun syncZone(): Boolean {
         val req = Request.Builder().url(ARK_ZONE).get().build()
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取zone数据失败" }
                     return false
                 }
@@ -142,10 +146,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                 zoneMap.clear()
                 zoneMap.putAll(temp)
                 log.info { "${"[DATA]获取zone数据成功, 共{}条"} ${zoneMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步zone数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步zone数据异常" }
+            return false
         }
 
         return true
@@ -153,10 +157,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     private fun syncActivity(): Boolean {
         val req = Request.Builder().url(ARK_ACTIVITY).get().build()
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取activity数据失败" }
                     return false
                 }
@@ -168,17 +172,17 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                 val temp = ConcurrentHashMap<String, ArkActivity>()
                 zoneToActivity.forEach { (zoneId, actId) ->
                     val act = baseInfos[actId]
-                    if (act != null) {
-                        temp[zoneId] = act
+                    act?.let {
+                        temp[zoneId] = it
                     }
                 }
                 zoneActivityMap.clear()
                 zoneActivityMap.putAll(temp)
                 log.info { "${"[DATA]获取activity数据成功, 共{}条"} ${zoneActivityMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步activity数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步activity数据异常" }
+            return false
         }
 
         return true
@@ -186,10 +190,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     private fun syncCharacter(): Boolean {
         val req = Request.Builder().url(ARK_CHARACTER).get().build()
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取character数据失败" }
                     return false
                 }
@@ -209,10 +213,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                     arkCharacterMap[ids[2]] = c
                 }
                 log.info { "${"[DATA]获取character数据成功, 共{}条"} ${arkCharacterMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步character数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步character数据异常" }
+            return false
         }
 
         return true
@@ -220,10 +224,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     private fun syncTower(): Boolean {
         val req = Request.Builder().url(ARK_TOWER).get().build()
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取tower数据失败" }
                     return false
                 }
@@ -232,10 +236,10 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
                 arkTowerMap.clear()
                 arkTowerMap.putAll(mapper.convertValue(towerNode, object : TypeReference<Map<String, ArkTower>>() {}))
                 log.info { "${"[DATA]获取tower数据成功, 共{}条"} ${arkTowerMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步tower数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步tower数据异常" }
+            return false
         }
 
         return true
@@ -243,25 +247,26 @@ class ArkGameDataService(private val okHttpClient: OkHttpClient) {
 
     fun syncCrisisV2Info(): Boolean {
         val req = Request.Builder().url(ARK_CRISIS_V2).get().build()
-        okHttpClient.newCall(req).execute().use { rsp ->
-            try {
+        try {
+            okHttpClient.newCall(req).execute().use { rsp ->
                 val body = rsp.body
-                if(body == null) {
+                if (body == null) {
                     log.error { "[DATA]获取crisisV2Info数据失败" }
                     return false
                 }
                 val node = mapper.reader().readTree(body.string())
                 val crisisV2InfoNode = node.get("seasonInfoDataMap")
-                val crisisV2InfoMap = mapper.convertValue(crisisV2InfoNode, object : TypeReference<Map<String, ArkCrisisV2Info>>() {})
+                val crisisV2InfoMap =
+                    mapper.convertValue(crisisV2InfoNode, object : TypeReference<Map<String, ArkCrisisV2Info>>() {})
                 val temp = ConcurrentHashMap<String, ArkCrisisV2Info>()
                 crisisV2InfoMap.forEach { (k, v) -> temp[ArkLevelUtil.getKeyInfoById(k)] = v }
                 arkCrisisV2InfoMap.clear()
                 arkCrisisV2InfoMap.putAll(temp)
                 log.info { "${"[DATA]获取crisisV2Info数据成功, 共{}条"} ${arkCrisisV2InfoMap.size}" }
-            } catch (e: Exception) {
-                log.error(e) { "[DATA]同步crisisV2Info数据异常" }
-                return false
             }
+        } catch (e: Exception) {
+            log.error(e) { "[DATA]同步crisisV2Info数据异常" }
+            return false
         }
 
         return true
