3e01fdb3b75fc07f610a6d0f12793c4218791ecc
KevinT3Hu <kevint3hu@outlook.com>
2024-02-07 08:44:28 +0800

    perf: remove several uses of java stream functions

--- src/main/java/plus/maa/backend/repository/GithubRepository.kt
+++ src/main/java/plus/maa/backend/repository/GithubRepository.kt
@@ -1,30 +1,29 @@
-package plus.maa.backend.repository;
+package plus.maa.backend.repository
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.service.annotation.GetExchange;
-import plus.maa.backend.repository.entity.github.GithubCommit;
-import plus.maa.backend.repository.entity.github.GithubContent;
-import plus.maa.backend.repository.entity.github.GithubTrees;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.service.annotation.GetExchange
+import plus.maa.backend.repository.entity.github.GithubCommit
+import plus.maa.backend.repository.entity.github.GithubContent
+import plus.maa.backend.repository.entity.github.GithubTrees
 
 /**
  * @author dragove
  * created on 2022/12/23
  */
-public interface GithubRepository {
-
+interface GithubRepository {
     /**
-     * api doc: <a href="https://docs.github.com/en/rest/git/trees?apiVersion=2022-11-28#get-a-tree">git trees api</a>
+     * api doc: [git trees api](https://docs.github.com/en/rest/git/trees?apiVersion=2022-11-28#get-a-tree)
      */
     @GetExchange(value = "/repos/MaaAssistantArknights/MaaAssistantArknights/git/trees/{sha}")
-    GithubTrees getTrees(@RequestHeader("Authorization") String token, @PathVariable("sha") String sha);
+    fun getTrees(@RequestHeader("Authorization") token: String, @PathVariable("sha") sha: String): GithubTrees
 
     @GetExchange(value = "/repos/MaaAssistantArknights/MaaAssistantArknights/commits")
-    List<GithubCommit> getCommits(@RequestHeader("Authorization") String token);
+    fun getCommits(@RequestHeader("Authorization") token: String): List<GithubCommit>
 
     @GetExchange(value = "/repos/MaaAssistantArknights/MaaAssistantArknights/contents/{path}")
-    List<GithubContent> getContents(@RequestHeader("Authorization") String token, @PathVariable("path") String path);
-
+    fun getContents(
+        @RequestHeader("Authorization") token: String,
+        @PathVariable("path") path: String
+    ): List<GithubContent>
 }
