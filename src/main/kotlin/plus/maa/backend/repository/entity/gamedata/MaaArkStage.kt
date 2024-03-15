06828bf34dd3f4170cf4f0824f98a317f035e43a
lixuhuilll <676824363@qq.com>
2024-03-15 15:59:28 +0800

    fix: 修复无法获取活动关卡开放状态的问题

--- src/main/kotlin/plus/maa/backend/repository/entity/gamedata/MaaArkStage.kt
+++ src/main/kotlin/plus/maa/backend/repository/entity/gamedata/MaaArkStage.kt
@@ -1,7 +1,11 @@
 package plus.maa.backend.repository.entity.gamedata
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.LowerCamelCaseStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 
+// 小驼峰
+@JsonNaming(LowerCamelCaseStrategy::class)
 // 忽略对服务器无用的数据
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class MaaArkStage(
