1c8cf5c5ec6618c7e62003e30652b688f7cb80c5
自由的世界人 <3196812536@qq.com>
2025-02-13 01:01:49 +0800

    feat: add user fuzzy search

--- src/main/kotlin/plus/maa/backend/repository/UserRepository.kt
+++ src/main/kotlin/plus/maa/backend/repository/UserRepository.kt
@@ -1,6 +1,8 @@
 package plus.maa.backend.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
+import plus.maa.backend.controller.response.user.MaaUserInfo
 import plus.maa.backend.repository.entity.MaaUser
 
 /**
@@ -16,4 +18,9 @@ interface UserRepository : MongoRepository<MaaUser, String> {
     fun findByEmail(email: String): MaaUser?
 
     fun findByUserId(userId: String): MaaUser?
+
+    @Query("{'\$or': [ " +
+        "{ 'userName': { \$eq: ?0 } }, " +
+        "] }")
+    fun searchUsers(userName: String): List<MaaUserInfo>
 }
