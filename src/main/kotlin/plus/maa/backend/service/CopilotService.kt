7c1c7c0cc8d3086bbe82b9e207455d65c0999e9a
Aliothmoon <aliothmoon@outlook.com>
2025-03-29 01:33:32 +0800

    fix: 查询内容只包含stop word

--- src/main/kotlin/plus/maa/backend/service/CopilotService.kt
+++ src/main/kotlin/plus/maa/backend/service/CopilotService.kt
@@ -11,7 +11,6 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.TextCriteria
-import org.springframework.data.mongodb.core.query.TextQuery
 import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Service
 import org.springframework.util.Assert
@@ -311,15 +310,14 @@ class CopilotService(
         }
 
         // 标题、描述、神秘代码
-        val queryObj = if (request.document.isNullOrBlank()) {
-            Query()
-        } else {
-            // 分词
-            val words = SegmentInfo.getSegment(request.document)
-            TextQuery(TextCriteria.forDefaultLanguage().matchingAny(*words.toTypedArray()))
-        }
+        val queryObj = Query().addCriteria(criteriaObj)
 
-        queryObj.addCriteria(criteriaObj)
+        if (!request.document.isNullOrBlank()) {
+            val words = SegmentInfo.getSegment(request.document)
+            if (words.isNotEmpty()) {
+                queryObj.addCriteria(TextCriteria.forDefaultLanguage().matchingAny(*words.toTypedArray()))
+            }
+        }
 
         // 去除large fields
         queryObj.fields().exclude("content", "actions", "segment")
